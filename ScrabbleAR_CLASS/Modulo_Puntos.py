1357981a4abf5382e72a37718d2df2daf125c955
Matias <Matias-DR@outlook.com>
2020-06-20 17:55:37 -0300

    Puntos agregados

---
+++ ScrabbleAR_CLASS/Modulo_Puntos.py
@@ -0,0 +1,35 @@
+from const import puntos_facil
+from const import puntos_medio
+from const import puntos_dificil
+from const import puntos_por_color
+
+
+class Puntos:
+    _dificultad = ''
+
+    def __init__(self, puntaje_inicial):
+        self._puntos = puntaje_inicial
+
+    def get_puntos(self):
+        return (self.puntos)
+
+    def set_puntos(self, puntaje):
+        self._puntos = puntaje
+
+    def get_dificultad(self):
+        return self._dificultad
+
+    def set_dificultad(self, dificultad):
+        self._dificultad = dificultad
+
+    def sumar(self, color):
+        if color is 'rojo':
+            self._puntos -= int(puntos_por_color[self._dificultad]['jugador']['puntos_rojo'][-1:])
+        elif color is 'verde':
+            self._puntos *= int(puntos_por_color[self._dificultad]['jugador']['puntos_verde'][-1:])
+        elif color is 'azul':
+            self._puntos += int(puntos_por_color[self._dificultad]['jugador']['puntos_azul'][-1:])
+        elif color is 'amarillo':
+            self._puntos += int(puntos_por_color[self._dificultad]['jugador']['puntos_amarillo'][-1:])
+        elif color is 'grey':
+            self._puntos += int(puntos_por_color[self._dificultad]['jugador']['puntos_gris'][-1:])
---
+++ ScrabbleAR_CLASS/const.py
@@ -0,0 +1,60 @@
+from PySimpleGUI import Window, Frame, Column, Button, Drop, Spin, Text
+from os import getcwd
+
+
+# ~~~~~~~~~~~~~~~ Funciones ~~~~~~~~~~~~~~~
+def top_diez_txt_format(data):
+    nuevo_top_diez = []
+    for top in data:
+        nuevo_top_diez.append('Puntos: {}\nFecha: {}\nDificultad: {}\n\n'.format(top['puntaje'], top['fecha'], top['dificultad']))
+    return nuevo_top_diez
+
+
+# ~~~~~~~~~~~~~~~ Archivos ~~~~~~~~~~~~~~~
+# JSON ~~~~~~~~~~~~~~~
+juego_guardado_json = 'archivos/juego_guardado.json'
+top_diez_json = 'archivos/top_diez.json'
+enlaces_json = [juego_guardado_json, top_diez_json]
+# TXT ~~~~~~~~~~~~~~~
+top_diez_txt = '{}\\archivos\\top_diez.txt'.format(getcwd())
+
+
+# ~~~~~~~~~~~~~~~ Parámetros del tablero ~~~~~~~~~~~~~~~
+llaves_tablero = [[(x, y) for x in range(15)] for y in range(15)]
+parametros = {'dificultad': '',
+              'puntos_jugador': 0,
+              'puntos_bot': 0,
+              'matriz': {}}
+puntos_facil = {'jugador': {'puntos_verde': ': x2',
+                            'puntos_azul': ': +4',
+                            'puntos_amarillo': ': +3',
+                            'puntos_gris': ': +2',
+                            'puntos_rojo': ': -1'},
+                'bot': {'puntos_verde': ': x2',
+                        'puntos_azul': ': +3',
+                        'puntos_amarillo': ': +2',
+                        'puntos_gris': ': +1',
+                        'puntos_rojo': ': -3'}}
+puntos_medio = {'jugador': {'puntos_verde': ': x2',
+                            'puntos_azul': ': +3',
+                            'puntos_amarillo': ': +2',
+                            'puntos_gris': ': +1',
+                            'puntos_rojo': ': -1'},
+                'bot': {'puntos_verde': ': x2',
+                        'puntos_azul': ': +3',
+                        'puntos_amarillo': ': +2',
+                        'puntos_gris': ': +1',
+                        'puntos_rojo': ': -1'}}
+puntos_dificil = {'jugador': {'puntos_verde': ': x1',
+                              'puntos_azul': ': +3',
+                              'puntos_amarillo': ': +2',
+                              'puntos_gris': ': +1',
+                              'puntos_rojo': ': -1'},
+                  'bot': {'puntos_verde': ': x2',
+                          'puntos_azul': ': +4',
+                          'puntos_amarillo': ': +3',
+                          'puntos_gris': ': +2',
+                          'puntos_rojo': ': -1'}}
+puntos_por_color = {'FÁCIL': puntos_facil,
+                    'MEDIO': puntos_medio,
+                    'DIFICIL': puntos_dificil}
